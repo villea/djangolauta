3c41aeb2c38cc5428b6b766ef00627283cfc89ed
Ville Anttonen <ext-ville.anttonen@elisa.fi>
2025-03-04 15:53:29 +0200

    fix post comment count

--- threads/models.py
+++ threads/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments',on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
